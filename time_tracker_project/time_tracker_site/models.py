322ca1bb034dbefced903be168c1bf0c93864cc3
Andrey Anokhin <anokhin.andrey@gmail.com>
2020-03-13 14:15:08 +0300

    add TimeTracking model

--- time_tracker_project/time_tracker_site/models.py
+++ time_tracker_project/time_tracker_site/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class TimeTracking(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150, unique=True)
+    hours = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.author, self.title)
