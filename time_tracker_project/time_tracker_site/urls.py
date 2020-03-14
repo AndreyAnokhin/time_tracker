d5b9585e5d1d00b10df806bd3617444fc4d62818
Andrey Anokhin <anokhin.andrey@gmail.com>
2020-03-14 11:10:23 +0300

    Add form for new task creating

--- time_tracker_project/time_tracker_site/urls.py
+++ time_tracker_project/time_tracker_site/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, TimeTrackerView, TaskCreateView
 
 urlpatterns = [
-    path('', index, name='index_url')
+    path('', index, name='index_url'),
+    path('time_tracker/', TimeTrackerView.as_view(), name='time_tracker_url'),
+    path('add_task/', TaskCreateView.as_view(), name='add_task_url'),
 ]
--- time_tracker_project/time_tracker_site/views.py
+++ time_tracker_project/time_tracker_site/views.py
@@ -1,4 +1,26 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import View, CreateView
+
+from .models import TimeTracking
+
+
+class TimeTrackerView(LoginRequiredMixin, View):
+    def get(self, request):
+        time_trackers = TimeTracking.objects.all()
+        return render(request, 'time_tracker_site/time_tracking.html', context={'time_trackers': time_trackers})
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = TimeTracking
+    fields = ['title', 'description', 'hours']
+    template_name = 'time_tracker_site/add_task.html'
+    success_url = reverse_lazy('time_tracker_url')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super(TaskCreateView, self).form_valid(form)
 
 
 def index(request):
