b6d3bf6f217ae2f338f0877eada15673510455cc
Andrey Anokhin <anokhin.andrey@gmail.com>
2020-03-14 11:08:41 +0300

    change model

--- time_tracker_project/time_tracker_site/models.py
+++ time_tracker_project/time_tracker_site/models.py
@@ -5,8 +5,8 @@ from django.conf import settings
 class TimeTracking(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=150, unique=True)
-    hours = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField()
+    hours = models.DecimalField(max_digits=6, decimal_places=2)
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
