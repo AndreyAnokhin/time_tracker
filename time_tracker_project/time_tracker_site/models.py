2408543c17ca0fb500cc46b9166fcda81eb12df1
Andrey Anokhin <anokhin.andrey@gmail.com>
2020-03-14 13:43:19 +0300

    add hours validation

--- time_tracker_project/time_tracker_site/models.py
+++ time_tracker_project/time_tracker_site/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.conf import settings
+from django.core.validators import MinValueValidator
+from decimal import Decimal
 
 
 class TimeTracking(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=150, unique=True)
     description = models.TextField()
-    hours = models.DecimalField(max_digits=6, decimal_places=2)
+    hours = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))])
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
