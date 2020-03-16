a46b9a0228e77f9c175268e3df367ff7e9c69f28
Andrey Anokhin <anokhin.andrey@gmail.com>
2020-03-16 12:32:16 +0300

    user get_absolute_url

--- time_tracker_project/time_tracker_site/models.py
+++ time_tracker_project/time_tracker_site/models.py
@@ -4,6 +4,7 @@ from decimal import Decimal
 from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.shortcuts import reverse
 from django.utils import timezone
 
 
@@ -12,10 +13,13 @@ class TimeTracking(models.Model):
     title = models.CharField(max_length=150, unique=True)
     description = models.TextField()
     hours = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))])
-    date_created = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateTimeField(auto_now_add=True, editable=True)
 
     def editable(self):
         return timezone.now() - self.date_created < datetime.timedelta(days=1)
+
+    def get_absolute_url(self):
+        return reverse('task_detail_url', kwargs={'task_id': self.id})
 
     def __str__(self):
         return '{} - {}'.format(self.author, self.title)
