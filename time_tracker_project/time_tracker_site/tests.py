53099e43d52b82a2c28266c0e17b6dc5618ed9da
Andrey Anokhin <anokhin.andrey@gmail.com>
2020-03-16 14:44:10 +0300

    add some tests

--- time_tracker_project/time_tracker_site/tests.py
+++ time_tracker_project/time_tracker_site/tests.py
@@ -1,3 +1,35 @@
+from datetime import date
+
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from .models import TimeTracking
+
+
+class TestIndexPage(TestCase):
+    def test_home_page_works(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'time_tracker_site/index.html')
+        self.assertContains(response, 'Time tracking web service')
+
+
+class TestTimeTrackingModel(TestCase):
+
+    def setUp(self):
+        self.test_user = User.objects.create_user('Test User', 'test@test.com', 'testpassword')
+
+    def test_whatever_creation(self):
+        project = TimeTracking.objects.create(
+            author=self.test_user,
+            title='project1',
+            description='project1 description',
+            hours=1)
+        self.assertTrue(isinstance(project, TimeTracking))
+        self.assertEqual('project1', project.title)
+        self.assertEqual('project1 description', project.description)
+        self.assertEqual(1, project.hours)
+        self.assertEqual(date.today(), project.date_created.date())
+
+    def tearDown(self):
+        self.test_user.delete()
